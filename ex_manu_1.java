00cfef7e3c61c8caa6e213baf072997cde0fe5de
Emersonmaj <107114958+Emersonmaj@users.noreply.github.com>
2022-06-08 08:57:02 -0300

    Add files via upload

---
+++ ex_manu_1.java
@@ -0,0 +1,24 @@
+package ex_manu_1;
+import java.util.Scanner;
+public class ex_manu_1 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+    int h,b,B,area;
+    System.out.println("digite a altura");
+    h= ler.nextInt();
+    System.out.println("digite a base menor");
+    b= ler.nextInt();
+    System.out.println("digite a base maior");
+    B= ler.nextInt();
+area = (h*(b + B))/2;
+    System.out.println("a area é: "+ area);  
+
+
+
+
+
+
+
+
+
+}}
