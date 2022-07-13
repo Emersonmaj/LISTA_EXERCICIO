03006566f0d2aa9fc079d093716b06a733ac3423
Emersonmaj <107114958+Emersonmaj@users.noreply.github.com>
2022-07-13 08:43:21 -0300

    Add files via upload

---
+++ Exercicio_3.java
@@ -0,0 +1,21 @@
+public class Exercicio_3 {
+    public static void main(String[]args) {
+    
+        float A, B, C, D;
+        System.out.println("a) 3 – 2 – 1 + 2 + 1 + 3 : ");
+        System.out.println("b) 2 * 3 – 4 * 5 : ");
+        System.out.println("c) 2 + 6 – 3 / 7 * 9 : ");
+        System.out.println("d) 3 % 4 – 8 : ");
+        A = ( 3 - 2 - 1 + 2 + 1 + 3 );
+        B = ((2 * 3) - (4 * 5));
+        C = ((2 + 6) - ((3 / 7) * 9));
+        D = (3 % (4 - 8)); 
+        System.out.println("a = " + A);
+        System.out.println("b = " + B);
+        System.out.println("c = " + C);
+        System.out.println("d = " + D);
+
+    }
+}
+    
+
